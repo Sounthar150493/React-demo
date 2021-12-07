08cbdb7a5c558823c72a79c130d3496efd201a50
Sounthar150493 <soundhar1008@gmail.com>
2021-12-07 12:30:25 +0530

    movie component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,54 +1,54 @@
-// import React, { Component } from 'react';
-// import { getMovies } from '../services/fakeMovieService';
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
 
-// class Movies extends Component {
-//   state = {
-//     movie: getMovies(),
-//   };
-//   handleDelete = (movie) => {
-//     const movies = this.state.movie.filter((m) => m._id !== movie._id);
-//     this.setState({ movie: movies });
-//   };
-//   render() {
-//     const { length: count } = this.state.movie;
-//     if (count === 0) {
-//       return <h3>There are no movies in the database!</h3>;
-//     }
-//     return (
-//       <div>
-//         <h4>Showing {count} in the database.</h4>
-//         <table className="table">
-//           <thead>
-//             <tr>
-//               <th>Title</th>
-//               <th>Genre</th>
-//               <th>Stock</th>
-//               <th>Rate</th>
-//               <th></th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {this.state.movie.map((movie) => (
-//               <tr key={movie._id}>
-//                 <td>{movie.title}</td>
-//                 <td>{movie.genre.name}</td>
-//                 <td>{movie.numberInStock}</td>
-//                 <td>{movie.dailyRentalRate}</td>
-//                 <td>
-//                   <button
-//                     onClick={() => this.handleDelete(movie)}
-//                     className="btn btn-warning btn-sm"
-//                   >
-//                     Remove
-//                   </button>
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     );
-//   }
-// }
+class Movies extends Component {
+  state = {
+    movie: getMovies(),
+  };
+  handleDelete = (movie) => {
+    const movies = this.state.movie.filter((m) => m._id !== movie._id);
+    this.setState({ movie: movies });
+  };
+  render() {
+    const { length: count } = this.state.movie;
+    if (count === 0) {
+      return <h3>There are no movies in the database!</h3>;
+    }
+    return (
+      <div>
+        <h4>Showing {count} in the database.</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movie.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-warning btn-sm"
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
-// export default Movies;
+export default Movies;
